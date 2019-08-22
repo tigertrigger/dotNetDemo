5089e9d6e4b9b16a450cb54f683981cb6f15f7ba
liyi2244369 <liyi2244369@163.com>
2019-08-22 17:58:28 +0800

    无刷新联级做完了

--- DnetDemo/控件进阶与AJAX/Update无刷新联级/Default.aspx
+++ DnetDemo/控件进阶与AJAX/Update无刷新联级/Default.aspx
@@ -11,14 +11,35 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <p style="color: green;">
+                选择省的时候，页面回发生成对应的市，但是地区需要在市区绑定后再生成，不然会出现如下：
+                <br />
+                湖南省 衡阳市 蒸湘区 =》湖北省 武汉市 蒸湘区。地区部分还是上一次衡阳市的回发生成的，因为页面生命周期只有一个databind
+                <br />
+                地区和市区都是在湖北省和衡阳市这个回发值生成的。
+            </p>
+            <p style="color: red;">解决方法：方法①选择省的时候把地区清空。方法②给市区加OnDataBound事件，意指databind事件后要干什么，这时在事件里对地区重新做数据绑定</p>
+        </div>
+        <div>
             <p class="showtime"><%=DateTime.Now.ToFileTime() %></p>
-            <asp:DropDownList ID="ddl_p" runat="server" DataSourceID="XmlDataSource1" DataTextField="Name" DataValueField="Id">
+            <asp:DropDownList ID="ddl_p" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddl_p_SelectedIndexChanged">
             </asp:DropDownList>
-            <asp:DropDownList ID="ddl_c" runat="server">
-            </asp:DropDownList>
-            <asp:DropDownList ID="ddl_d" runat="server">
-            </asp:DropDownList>
-            <asp:XmlDataSource ID="XmlDataSource1" runat="server" DataFile="~/控件进阶与AJAX/Update无刷新联级/province.xml"></asp:XmlDataSource>
+
+
+            <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Conditional" RenderMode="Inline">
+                <ContentTemplate>
+                    <asp:DropDownList ID="ddl_c" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddl_c_SelectedIndexChanged"
+                        OnDataBound="reload_d">
+                    </asp:DropDownList>
+                    <asp:DropDownList ID="ddl_d" runat="server">
+                    </asp:DropDownList>
+                    <p class="showtime"><%=DateTime.Now.ToFileTime() %></p>
+                </ContentTemplate>
+                <Triggers>
+                    <asp:AsyncPostBackTrigger ControlID="ddl_p" />
+                </Triggers>
+            </asp:UpdatePanel>
         </div>
     </form>
 </body>
--- DnetDemo/控件进阶与AJAX/Update无刷新联级/Default.aspx.cs
+++ DnetDemo/控件进阶与AJAX/Update无刷新联级/Default.aspx.cs
@@ -12,13 +12,80 @@ public partial class 控件进阶与AJAX_Update无刷新联级_Default : System.
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        if (!IsPostBack)
+        {
+            DataTable tb_p = CXmlToDataTable("~/控件进阶与AJAX/Update无刷新联级/province.xml", 0);
+            ddl_p.DataSource = tb_p;
+            ddl_p.DataTextField = "name";
+            ddl_p.DataValueField = "id";
+            ddl_p.DataBind();
+            ListItem item = new ListItem();
+            item.Text = "请选择";
+            item.Value = "00";
+            item.Selected = true;
+            ddl_p.Items.Insert(0, item);
+        }
     }
 
     protected void ddl_p_SelectedIndexChanged(object sender, EventArgs e)
     {
-
+        string id = ddl_p.SelectedValue.ToString();
+        child(id, "c");
+        //ddl_d.Items.Clear();
     }
+
+    protected void child(string p_id, string cd)
+    {
+        DataTable tb_c;
+        if (cd == "c")
+        {
+            tb_c = CXmlToDataTable("~/控件进阶与AJAX/Update无刷新联级/city.xml", 0);
+        }
+        else
+        {
+            tb_c = CXmlToDataTable("~/控件进阶与AJAX/Update无刷新联级/district.xml", 0);
+        }
+        DataTable tb_sc = tb_c.Clone();
+        foreach (DataRow row in tb_c.Rows)
+        {
+            if (row["p_id"].ToString() == p_id)
+            {
+                tb_sc.ImportRow(row);
+            }
+        }
+        if (tb_sc != null && tb_sc.Rows.Count > 0)
+        {
+            DropDownList ddl = (DropDownList)FindControl("ddl_" + cd);
+            ddl.DataSource = tb_sc;
+            ddl.DataTextField = "name";
+            ddl.DataValueField = "id";
+            ddl.DataBind();
+            //ListItem lt = new ListItem();
+            //lt.Text = "请选择";
+            //lt.Value = "00";
+            //lt.Selected = true;
+            //ddl.Items.Insert(0, lt);
+        }
+        else
+        {
+            ddl_c.Items.Clear();
+            ddl_d.Items.Clear();
+        }
+    }
+
+    protected void ddl_c_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string id = ddl_c.SelectedValue.ToString();
+        child(id, "d");
+    }
+
+    protected void reload_d(object sender ,EventArgs e)
+    {
+        string id = ddl_c.SelectedValue.ToString();
+        child(id, "d");
+    }
+
+
     public static DataTable CXmlToDataTable(string xmlFilePath, int tableIndex)
     {
         return CXmlFileToDataSet(xmlFilePath).Tables[tableIndex];
@@ -65,4 +132,5 @@ public partial class 控件进阶与AJAX_Update无刷新联级_Default : System.
             return null;
         }
     }
+
 }
